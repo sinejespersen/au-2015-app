bf8bc87e79786e1d12b909d58fa2563ecfa4097a
--replace-all <sinejespersen@gmail.com>
2015-10-31 17:50:47 +0100

    build

---
+++ build/generated/source/buildConfig/release/dk/iha/itsmap/e15/grp11/SV/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dk.iha.itsmap.e15.grp11.SV;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "dk.iha.itsmap.e15.grp11.SV";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
